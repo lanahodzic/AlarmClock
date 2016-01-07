91fc89d6a718d61d3b28d0a097dabc85039ba934
lanahodzic <lana@enoteway.com>
2016-01-07 13:10:30 -0800

    Initial project state.

--- AlarmClock/ViewController.swift
+++ AlarmClock/ViewController.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AlarmCell: UITableViewCell {
+    
+    @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var state: UISwitch!
+}
 
+class ViewController: UITableViewController {
+
+    
+    @IBOutlet var alarm_table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +28,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        
+        return UITableViewCell()
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return alarm_table.numberOfRowsInSection(section)
+    }
+    
+    
+    
 
 }
 
